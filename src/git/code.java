57fec5e4bce2e9ac061a4d7966764b0b87535bd7
Suman <sumankumari.xiss@gmail.com>
2019-12-24 22:25:20 -0800

    init changes

---
+++ src/git/code.java
@@ -0,0 +1,14 @@
+package git;
+
+public class code {
+
+	public static void main(String[] args) {
+
+		int a = 10;
+		int b = 5;
+		int c = a + b;
+		System.out.println(c);
+
+	}
+
+}
---
+++ src/git/code1.java
@@ -0,0 +1,10 @@
+package git;
+
+public class code1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is java programming");
+	}
+
+}
